692e7f573f4a80a09b21fba90b744040381aba6c
deanmastria <dean.mastria@gmail.com>
2024-07-31 19:19:30 -0400

    tasks 1,3 and 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,20 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Task 1: Define a List with at least one set of duplicates
+        List<Integer> integerList = new ArrayList<>(1,2,3,4,5,6,7,8,9,9);
+
+        // Instance of the IntegerListOperations class and passing the integerList as an argument to its constructor
+        IntegerListOperations operations = new IntegerListOperations(integerList);
+
+        // Task 3: Sum and Average with dupes
+        System.out.printf("Sum" + operations.getSum());
+        System.out.println("Average" + operations.getAverage());
+
+        // Task 4: Sum, Average and List of integers without dupes
+        System.out.printf("Sum of unique integers" + operations.getSumOfUniqueIntegers());
+        System.out.printf("Avg of unique integers" + operations.getAverageOfUniqueIntegers());
+        System.out.printf("List of unique integers" + operations.getUniqueIntegers());
         }
     }
-}
